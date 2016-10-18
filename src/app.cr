dca2e79b740305b6729ecaff30df8572732f5f2a
ckndev <ckndev@gmail.com>
2016-10-18 12:37:04 +0200

    Adds dependencies requirements in app entry point

--- src/app.cr
+++ src/app.cr
@@ -1,5 +1,13 @@
 require "./app/*"
+require "./app/entity/*"
+require "./app/repository/*"
+require "./app/view/*"
+require "./app/factory_view/*"
+require "./app/request/*"
+require "./app/response/*"
+require "./app/validator/*"
+require "./app/use_case/*"
+
 
 module App
-  # TODO Put your code here
 end
